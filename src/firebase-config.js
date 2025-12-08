25cf93ab40c8b6c2f20fe4a8abf50c1fafe3216d
pathummalsara93-ux <pathummalsara93@gmail.com>
2025-12-08 20:36:33 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsMM3Z-tGNfgZTlAO9Tr21nlyhZL8jUuk",
-  authDomain: "film-hub-official.firebaseapp.com",
-  projectId: "film-hub-official",
-  storageBucket: "film-hub-official.firebasestorage.app",
-  messagingSenderId: "863017368992",
-  appId: "1:863017368992:web:80ee942d60ba64b5de78c5",
-  measurementId: "G-W77J8NFFR7"
+  apiKey: "AIzaSyApzzYTbywpe5Lf-CJ6zNmYk6PLZ3d77c8",
+  authDomain: "film-hub-2026.firebaseapp.com",
+  projectId: "film-hub-2026",
+  storageBucket: "film-hub-2026.firebasestorage.app",
+  messagingSenderId: "580032384676",
+  appId: "1:580032384676:web:16e6057bb6a0bb5dbb2031",
+  measurementId: "G-K67EDQJ62S"
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);

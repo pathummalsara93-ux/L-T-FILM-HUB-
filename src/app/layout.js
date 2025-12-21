56c888713834c8538661bba3cc737d6fd7adc12f
pathummalsara93-ux <pathummalsara93@gmail.com>
2025-12-21 10:55:41 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,15 @@
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./components/Providers";
+import Loading from "./loading";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  applicationName: 'MOVIE-REQUEST',
+  applicationName: "MOVIE-REQUEST",
   title: "MOVIE-REQUEST",
   description:
     "A simple realtime chat application without authentication created using Next.js and Firebase",
@@ -21,18 +25,32 @@ export const metadata = {
 };
 
 export const viewport = {
-  themeColor: 'black',
-  width: 'device-width',
+  themeColor: "black",
+  width: "device-width",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-}
+};
 
 export default function RootLayout({ children }) {
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLoader(false);
+    }, 2000); // ⬅️ loader time (ms)
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
+      <body className={inter.className + " overflow-hidden"}>
+        {showLoader ? (
+          <Loading />
+        ) : (
+          <Providers>{children}</Providers>
+        )}
       </body>
     </html>
   );
